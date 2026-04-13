7303de0604aca623a701d61b940f133faa093895
zzrr41980-dot <zzrr41980@gmail.com>
2026-04-13 14:46:23 +0300

    Update api_config.dart

--- flutter_app/lib/config/api_config.dart
+++ flutter_app/lib/config/api_config.dart
@@ -1,10 +1,19 @@
 import 'package:flutter/foundation.dart';
+const String kAndroidEmulatorApiBase = 'http://10.0.2.2:8080';
 
-/// عنوان الخادم الافتراضي حسب المنصة (المحاكي Android يستخدم 10.0.2.2 بدل localhost).
+const String _apiBaseFromEnv = String.fromEnvironment(
+  'TERYAQI_API_BASE',
+  defaultValue: '',
+);
+
 String defaultApiBaseUrl() {
   if (kIsWeb) return 'http://localhost:8080';
   if (defaultTargetPlatform == TargetPlatform.android) {
-    return 'http://10.0.2.2:8080';
+    final fromEnv = _apiBaseFromEnv.trim();
+    if (fromEnv.isNotEmpty) {
+      return fromEnv.replaceAll(RegExp(r'/$'), '');
+    }
+    return kAndroidEmulatorApiBase;
   }
   return 'http://127.0.0.1:8080';
 }
