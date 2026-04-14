c43fccc13beb2c6258e0b45c66eb8705e52b1169
zzrr41980-dot <zzrr41980@gmail.com>
2026-04-14 15:15:03 +0300

    Update api_config.dart

--- flutter_app/lib/config/api_config.dart
+++ flutter_app/lib/config/api_config.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/foundation.dart';
-const String kAndroidEmulatorApiBase = 'http://10.0.2.2:8080';
+const String kAndroidEmulatorApiBase = 'http://10.0.2.2/Teryaqi-main';
 
 const String _apiBaseFromEnv = String.fromEnvironment(
   'TERYAQI_API_BASE',
